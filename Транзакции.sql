68d75a90441f93466bba13b39e923b62b3b04907
AlexanderTsankov <20102@uktc-bg.com>
2024-05-27 18:49:43 +0300

    Update Транзакции.sql

--- Транзакции.sql
+++ Транзакции.sql
@@ -1,11 +1,11 @@
 START TRANSACTION;
 
 UPDATE Accounts
-SET Balance = Balance + 100
+SET Balance = Balance + 1000
 WHERE AccountID = 1;
 
 INSERT INTO Transactions (AccountID, TransactionType, Amount, TransactionDate)
-VALUES (1, 'Депозит', 100, CURDATE());
+VALUES (1, 'Депозит', 1000, CURDATE());
 
 COMMIT;
 
